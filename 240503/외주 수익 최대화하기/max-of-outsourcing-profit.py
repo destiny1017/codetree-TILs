fd43bde3f7790d355c6937f7167d6696d474ec64
KimDaeho <eogh6428@naver.com>
2024-05-03 22:09:35 +0900

    Title: 외주 수익 최대화하기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240503/외주 수익 최대화하기/max-of-outsourcing-profit.py
+++ 240503/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -4,7 +4,7 @@ n = int(sys.stdin.readline())
 jobs = []
 result = 0
 
-for i in range(1, n+1):
+for i in range(n):
     jobs.append(tuple(map(int, sys.stdin.readline().split())))
 
 dp = [0] * (n)
@@ -12,10 +12,10 @@ dp = [0] * (n)
 for i in range(n):
     for j in range(i+1):
         if j + jobs[j][0] - 1 == i:
-            # print(i, j)
+            # print(i, j, " > ", dp[i - jobs[j][0]], " + ", jobs[j][1])
             dp[i] = max(dp[i], dp[i - jobs[j][0]] + jobs[j][1])
-    if dp[i] == 0:
-        dp[i] = dp[i-1]
+    # if dp[i] == 0:
+    #     dp[i] = dp[i-1]
 
 # print(dp)
 print(max(dp))
