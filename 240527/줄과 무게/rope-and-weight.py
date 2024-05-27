75e477b294b26ba70b157ad6fb64c641b8f65e82
KimDaeho <eogh6428@naver.com>
2024-05-27 23:48:47 +0900

    Title: 줄과 무게, Time: 154ms, Memory: 31MB, Status: Passed - CodeTree

--- 240527/줄과 무게/rope-and-weight.py
+++ 240527/줄과 무게/rope-and-weight.py
@@ -1,20 +1,15 @@
 import sys
 n = int(sys.stdin.readline())
-lines = [int(sys.stdin.readline()) for _ in range(n)]
+
+lines = list(int(sys.stdin.readline()) for _ in range(n))
 lines.sort()
-prev = 0
 
-for i in range(len(lines)):
+max_w = lines[0] * n
 
+for i in range(1, n):
     if lines[i] == lines[i-1]:
         continue
-
-    now = lines[i] * (n-i)
     
-    if now < prev:
-        break
+    max_w = max(max_w, lines[i] * (n-i))
 
-    prev = now
- 
-
-print(prev)
+print(max_w)
