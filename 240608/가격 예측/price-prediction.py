fb0e3a4a50f98783ed63bcea8beffb1adda2e3a3
KimDaeho <eogh6428@naver.com>
2024-06-08 23:09:53 +0900

    Title: 가격 예측, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240608/가격 예측/price-prediction.py
@@ -0,0 +1,37 @@
+import sys
+
+'''
+1. 저점매수 고점매도 반복
+2. 저점지점과 고점지점 파악하여 최대한 빈번한 매도 수행
+3. 다음 가격이 떨어지면 전체 매도, 올라가면 전체 매수
+'''
+
+n, w = map(int, input().split())
+
+price = [int(input()) for _ in range(n)]
+
+amount = 0
+last_buy = 0
+buy_flag = False
+
+for i in range(n-1):
+    today, tomorrow = price[i], price[i+1]
+    if tomorrow > today: # 내일이 더 크면 매수
+        if buy_flag:
+            continue
+        amount = w // today
+        w %= today
+        last_buy = today
+        buy_flag = True
+    else: # 내일이 더 작으면 매도
+        w = w + (today * amount)
+        amount = 0
+        buy_flag = False
+    #print(today, w, amount)
+
+if amount > 0 and price[-1] > price[-2]:
+    w += price[-1] * amount
+else:
+    w += last_buy * amount
+
+print(w)
