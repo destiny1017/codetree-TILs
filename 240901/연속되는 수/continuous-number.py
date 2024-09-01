5d0e8e2dc021904e48779257de1969d567663059
KimDaeho <eogh6428@naver.com>
2024-09-01 15:04:28 +0900

    Title: 연속되는 수, Time: 120ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240901/연속되는 수/continuous-number.py
+++ 240901/연속되는 수/continuous-number.py
@@ -10,7 +10,7 @@ for i in range(n):
     arr.append(int(input()))
 
 
-max_cnt = 0
+max_cnt = 1
 
 for num in arr:
     tmp = arr.copy()
@@ -18,19 +18,19 @@ for num in arr:
         if tmp[i] == num:
             tmp[i] = -1
 
-    repeat_cnt = 0
-    jump_cnt = 0
-    for i in range(len(tmp)-1):
+    repeat_cnt = 1
+    jump_cnt = 1
+    for i in range(1, len(tmp)):
         if tmp[i] == -1:
             jump_cnt += 1
             continue
 
-        if tmp[i] == tmp[i+jump_cnt]:
+        if tmp[i] == tmp[i-jump_cnt]:
             repeat_cnt += 1
         else:
             max_cnt = max(repeat_cnt, max_cnt)
-            repeat_cnt = 0
+            repeat_cnt = 1
 
-        jump_cnt = 0
+        jump_cnt = 1
 
 print(max_cnt)
