4a467c5f707ddf669b640dcdf81bf5f621491f47
KimDaeho <eogh6428@naver.com>
2024-09-03 21:02:15 +0900

    Title: 연쇄로 터지는 폭탄, Time: 109ms, Memory: 25MB, Status: Passed - CodeTree

--- 240903/연쇄로 터지는 폭탄/series-of-bombs-detonating.py
+++ 240903/연쇄로 터지는 폭탄/series-of-bombs-detonating.py
@@ -7,14 +7,14 @@ bombs = [int(input()) for _ in range(n)]
 
 def boom_left(start, step, total_cnt, bombs):
     boom_range = start - step
+    next_bomb = 0
     cnt = 0
     
     # 폭발 범위까지 폭발 반복
     while bombs:
-        next_bomb = bombs.pop()
-        if next_bomb < boom_range:
-            bombs.append(next_bomb)
+        if bombs[-1] < boom_range:
             break
+        next_bomb = bombs.pop()
         cnt += 1
 
     # 마지막 터진 폭탄이 있으면 범위 늘려서 재귀호출
@@ -34,7 +34,7 @@ def boom_right(start, step, total_cnt, bombs):
             break
         next_bomb = bombs.pop()
         cnt += 1
-    # print(start, step, cnt)
+
     # 마지막 터진 폭탄이 있으면 범위 늘려서 재귀호출
     if cnt > 0:
         return boom_right(next_bomb, step+1, total_cnt + cnt, bombs)
@@ -46,10 +46,9 @@ max_cnt = 0
 
 for i in range(len(bombs)):
     bomb = bombs[i]
-    # cnt = boom_left(bomb, 1, 0, bombs[:i]) + boom_right(bomb, 1, 0, bombs[i:])
     left_cnt = boom_left(bomb, 1, 0, bombs[:i])
     right_cnt = boom_right(bomb, 1, 0, sorted(bombs[i+1:], reverse=True))
-    # print(f"bomb = {bomb}, left_cnt={left_cnt}, right_cnt={right_cnt}")
+
     max_cnt = max(max_cnt, left_cnt + right_cnt + 1)
 
 print(max_cnt)
