f771dce28376248a76f1c20071cfc0a2a713fbe1
KimDaeho <eogh6428@naver.com>
2024-10-22 22:51:18 +0900

    Title: 스승의 은혜, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 241022/스승의 은혜/the-grace-form-teacher.py
+++ 241022/스승의 은혜/the-grace-form-teacher.py
@@ -1,25 +1,23 @@
 n, b = map(int, input().split())
 costs = []
 cnt = 0
-discount = False
 
 for i in range(n):
     product, ship = map(int, input().split())
     costs.append((product, ship))
 
-costs.sort()
-for cost in costs:
+costs.sort(key=lambda x: x[0] + x[1])
+i = 0
+for i in range(len(costs)):
+    cost = costs[i]
     total = cost[0] + cost[1]
     if b >= total:
         b -= total
         cnt += 1
     else:
-        if not discount:
-            total = cost[0] // 2 + cost[1]
-            if b >= total:
-                b -= total
-                cnt += 1
-                discount = True
-        break
+        if cost[0] // 2 + cost[1] <= b:
+            cnt += 1
+            break
+
 
 print(cnt)
