2480b7200cd5ce0e6f2d8fa1404bac5e955e34d7
KimDaeho <eogh6428@naver.com>
2025-02-15 23:52:12 +0900

    Title: 대응되는 수와 문자, Time: 271ms, Memory: 39MB, Status: Passed - CodeTree

--- 250215/대응되는 수와 문자/corresponding-numbers-and-characters.py
+++ 250215/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -1,13 +1,18 @@
 n, m = map(int, input().split())
 
 # Note: Using 1-based indexing for words as per C++ code
-words = [""] + [input() for _ in range(n)]
+words_map = {}
+words = [""]
+for i in range(1, n+1):
+    word = input()
+    words.append(word)
+    words_map[word] = i
+
 queries = [input() for _ in range(m)]
 
 # Write your code here!
-
 for q in queries:
     if q.isdigit():
         print(words[int(q)])
     else:
-        print(words.index(q))
+        print(words_map[q])
